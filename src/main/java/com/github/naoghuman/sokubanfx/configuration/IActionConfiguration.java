1f88f4e907e09c941729408d80b17776fcaeb61c
PRo <peter.rogge@yahoo.de>
2016-04-24 23:49:41 +0200

    Change autor.

--- src/main/java/com/github/naoghuman/sokubanfx/configuration/IActionConfiguration.java
+++ src/main/java/com/github/naoghuman/sokubanfx/configuration/IActionConfiguration.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2016 PRo
+ * Copyright (C) 2016 Naoghuman
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -18,7 +18,7 @@ package com.github.naoghuman.sokubanfx.configuration;
 
 /**
  *
- * @author PRo
+ * @author Naoghuman
  */
 public interface IActionConfiguration {
     
