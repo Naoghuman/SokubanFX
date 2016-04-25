f5ee16f154381a3078bdab818912bd33d5d4edeb
PRo <peter.rogge@yahoo.de>
2016-04-25 12:17:40 +0200

    Change autor.

--- src/test/java/com/github/naoghuman/sokubanfx/map/collision/CollisionCheckerTest.java
+++ src/test/java/com/github/naoghuman/sokubanfx/map/collision/CollisionCheckerTest.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2016 PRo
+ * Copyright (C) 2016 Naoghuman
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -28,7 +28,7 @@ import static org.junit.Assert.*;
 
 /**
  *
- * @author PRo
+ * @author Naoghuman
  */
 public class CollisionCheckerTest {
     
