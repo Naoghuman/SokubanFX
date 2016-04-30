49fb7f8c3f6002156795d3f0dc5550ce60249a7f
PRo <peter.rogge@yahoo.de>
2016-04-30 22:13:13 +0200

    Preview shows now a new random map automatically after xy sec delay.

--- src/main/java/com/github/naoghuman/sokubanfx/view/preview/PreviewPresenter.java
+++ src/main/java/com/github/naoghuman/sokubanfx/view/preview/PreviewPresenter.java
@@ -25,12 +25,18 @@ import com.github.naoghuman.sokubanfx.map.converter.MapConverter;
 import java.net.URL;
 import java.util.List;
 import java.util.ResourceBundle;
+import javafx.animation.Animation;
+import javafx.animation.FadeTransition;
+import javafx.animation.PauseTransition;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Label;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.VBox;
 import javafx.scene.text.Font;
+import javafx.util.Duration;
 
 /**
  *
@@ -43,6 +49,8 @@ public class PreviewPresenter implements Initializable, IActionConfiguration {
     @FXML private ImageView iv;
     @FXML private VBox vbRandomMap;
     
+    private PauseTransition ptShowNextRandomMap;
+    
 //    public static final String PATH_TO_FOLDER__RESOURCES_IMAGES_MAPMARKER__WITH_FILE =
 //            "file:" // NOI18N
 //            + "resources" // NOI18N
@@ -52,31 +60,36 @@ public class PreviewPresenter implements Initializable, IActionConfiguration {
     
     @Override
     public void initialize(URL location, ResourceBundle resources) {
+        LoggerFacade.INSTANCE.debug(this.getClass(), "Initialize PreviewPresenter"); // NOI18N
+        
+        this.initializeShowNextRandomMap();
+        
+        this.onActionNextRandomMap();
         
         // XXX Test
-        System.out.println("++++++++++++++++++++++++++++++++++++++++++++++++++++");
-        final int randomMapIndex = MapFacade.INSTANCE.getRandomMapIndex();
-        final MapModel mm = MapFacade.INSTANCE.loadMap(randomMapIndex);
-        System.out.println("lvl: " + mm.getLevel());
-        System.out.println("col: " + mm.getColumns());
-        System.out.println("row: " + mm.getRows());
-        
-        final MapConverter mc = new MapConverter();
-        final List<String> map1 = mc.convertMapCoordinatesToStrings(mm);
-        map1.stream().forEach(line -> {
-            System.out.println(line);
-        });
-        
-        System.out.println("++++++++++++++++++++++++++++++++++++++++++++++++++++");
-        
-        vbRandomMap.getChildren().clear();
-        vbRandomMap.getChildren().add(this.getLabel("Map: " + randomMapIndex));
-        vbRandomMap.getChildren().add(this.getLabel(""));
-        
-        final List<String> map = mm.getMapAsStrings();
-        map.stream().forEach(line -> {
-            vbRandomMap.getChildren().add(this.getLabel(line));
-        });
+//        System.out.println("++++++++++++++++++++++++++++++++++++++++++++++++++++");
+//        final int randomMapIndex = MapFacade.INSTANCE.getRandomMapIndex();
+//        final MapModel mm = MapFacade.INSTANCE.loadMap(randomMapIndex);
+//        System.out.println("lvl: " + mm.getLevel());
+//        System.out.println("col: " + mm.getColumns());
+//        System.out.println("row: " + mm.getRows());
+//        
+//        final MapConverter mc = new MapConverter();
+//        final List<String> map1 = mc.convertMapCoordinatesToStrings(mm);
+//        map1.stream().forEach(line -> {
+//            System.out.println(line);
+//        });
+//        
+//        System.out.println("++++++++++++++++++++++++++++++++++++++++++++++++++++");
+//        
+//        vbRandomMap.getChildren().clear();
+//        vbRandomMap.getChildren().add(this.getLabel("Map: " + randomMapIndex));
+//        vbRandomMap.getChildren().add(this.getLabel(""));
+//        
+//        final List<String> map = mm.getMapAsStrings();
+//        map.stream().forEach(line -> {
+//            vbRandomMap.getChildren().add(this.getLabel(line));
+//        });
         
 //        Image img = null;
 //        try {
@@ -91,6 +104,25 @@ public class PreviewPresenter implements Initializable, IActionConfiguration {
 //        iv.setImage(img);
     }
     
+    private void initializeShowNextRandomMap() {
+        LoggerFacade.INSTANCE.debug(this.getClass(), " Initialize show next random Map"); // NOI18N
+        
+        if (
+                ptShowNextRandomMap != null
+                && ptShowNextRandomMap.getStatus().equals(Animation.Status.RUNNING)
+        ) {
+            ptShowNextRandomMap.stop();
+        }
+        
+        ptShowNextRandomMap = new PauseTransition();
+        ptShowNextRandomMap.setDuration(Duration.seconds(5.0d));
+        ptShowNextRandomMap.setOnFinished((ActionEvent event) -> {
+            this.onActionHideMap();
+        });
+        
+        ptShowNextRandomMap.playFromStart();
+    }
+    
     private Label getLabel(String text) {
         final Label label = new Label(text);
         label.setFont(new Font("Monospaced Regular", 18.0d));
@@ -98,12 +130,59 @@ public class PreviewPresenter implements Initializable, IActionConfiguration {
         return label;
     }
     
-    public void onActionNextRandomMap() {
-        LoggerFacade.INSTANCE.debug(this.getClass(), "On action next random map"); // NOI18N
+    private void onActionHideMap() {
+        final FadeTransition ftHideMap = new FadeTransition();
+        ftHideMap.setDuration(Duration.millis(250.0d));
+        ftHideMap.setFromValue(1.0d);
+        ftHideMap.setToValue(0.0d);
+        ftHideMap.setNode(vbRandomMap);
+        ftHideMap.setOnFinished((ActionEvent event) -> {
+            this.onActionNextRandomMap();
+            this.initializeShowNextRandomMap();
+        });
         
-        System.out.println("++++++++++++++++++++++++++++++++++++++++++++++++++++");
+        ftHideMap.playFromStart();
+    }
+    
+    public void onActionNextRandomMap() {
+        LoggerFacade.INSTANCE.debug(this.getClass(), "On action next random Map"); // NOI18N
+        
         final int randomMapIndex = MapFacade.INSTANCE.getRandomMapIndex();
         final MapModel mm = MapFacade.INSTANCE.loadMap(randomMapIndex);
+        this.printMapToConsole(mm);
+        
+        vbRandomMap.getChildren().clear();
+        vbRandomMap.setOpacity(0.0d);
+        vbRandomMap.getChildren().add(this.getLabel("Map: " + randomMapIndex)); // NOI18N
+        vbRandomMap.getChildren().add(this.getLabel("")); // NOI18N
+        
+        final List<String> map = mm.getMapAsStrings();
+        map.stream().forEach(line -> {
+            vbRandomMap.getChildren().add(this.getLabel(line));
+        });
+        
+        final FadeTransition ftShowNextRandomMap = new FadeTransition();
+        ftShowNextRandomMap.setDuration(Duration.millis(375.0d));
+        ftShowNextRandomMap.setFromValue(0.0d);
+        ftShowNextRandomMap.setToValue(1.0d);
+        ftShowNextRandomMap.setNode(vbRandomMap);
+        
+        ftShowNextRandomMap.playFromStart();
+    }
+    
+    public void onActionStartGame() {
+        LoggerFacade.INSTANCE.debug(this.getClass(), "On action start Game"); // NOI18N
+        
+        if (ptShowNextRandomMap.getStatus().equals(Animation.Status.RUNNING)) {
+            ptShowNextRandomMap.stop();
+        }
+        
+        ActionFacade.INSTANCE.handle(ON_ACTION__CHANGE_TO_GAMEVIEW);
+    }
+    
+    private void printMapToConsole(MapModel mm) {
+        System.out.println("++++++++++++++++++++++++++++++++++++++++++++++++++++");
+        
         System.out.println("lvl: " + mm.getLevel());
         System.out.println("col: " + mm.getColumns());
         System.out.println("row: " + mm.getRows());
@@ -115,21 +194,6 @@ public class PreviewPresenter implements Initializable, IActionConfiguration {
         });
         
         System.out.println("++++++++++++++++++++++++++++++++++++++++++++++++++++");
-        
-        vbRandomMap.getChildren().clear();
-        vbRandomMap.getChildren().add(this.getLabel("Map: " + randomMapIndex));
-        vbRandomMap.getChildren().add(this.getLabel(""));
-        
-        final List<String> map = mm.getMapAsStrings();
-        map.stream().forEach(line -> {
-            vbRandomMap.getChildren().add(this.getLabel(line));
-        });
-    }
-    
-    public void onActionStartGame() {
-        LoggerFacade.INSTANCE.debug(this.getClass(), "On action start Game"); // NOI18N
-        
-        ActionFacade.INSTANCE.handle(ON_ACTION__CHANGE_TO_GAMEVIEW);
     }
     
 }
