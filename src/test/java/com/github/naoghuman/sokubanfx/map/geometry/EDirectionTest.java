b150307071e3c18466fba995a31a38611d15a650
PRo <peter.rogge@yahoo.de>
2016-05-03 18:51:02 +0200

    Refactore package -.geometry to -.map.geometry.

--- src/test/java/com/github/naoghuman/sokubanfx/map/geometry/EDirectionTest.java
+++ src/test/java/com/github/naoghuman/sokubanfx/map/geometry/EDirectionTest.java
@@ -14,7 +14,7 @@
  * You should have received a copy of the GNU General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
-package com.github.naoghuman.sokubanfx.mapgeometry;
+package com.github.naoghuman.sokubanfx.map.geometry;
 
 import com.github.naoghuman.sokubanfx.map.geometry.EDirection;
 import org.junit.Test;
